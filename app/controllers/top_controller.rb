4df1355e425d42a4910c975b0e6b684ca483e65b
Junya Wako <j.wako@j-q.co.jp>
2014-08-09 12:21:29 +0900

    Refactoring calc_points

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,14 +4,9 @@ class TopController < ApplicationController
   end
 
   def result
-  	# "answer"=>{"1"=>{"2"=>"true"}, "2"=>{"3"=>"true"}, "3"=>{"3"=>"true"}, "4"=>{"2"=>"true"}, "5"=>{"2"=>"true"}, "6"=>{"3"=>"true"}}
-  	@cities, sum = {}, 0
-  	Municipality.all.each do |city|
-  		sum = 0
-  		sum = city.open_data.inject(0) { |sum, i| i.deviation_value * params[:answers][i.indicator_source.indicator.category.id.to_s].keys.first.to_i }
-  		sum /= city.open_data.size
-  		@cities.store(city.name, sum)
-  	end
+    return redirect_to top_path, alert: "重視する項目を選択してください" if params[:weights].blank?
+  	@cities = {}
+  	Municipality.with_includes.order(:id).map { |city| @cities.store(city.name, city.calc_point(params[:weights])) }
   	@cities = @cities.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
   end
 
--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -1,4 +1,18 @@
 class Municipality < ActiveRecord::Base
 	has_many :open_data, dependent: :destroy
 
+	scope :with_includes, -> { includes(:open_data => {:indicator_source => {:indicator => :category}}) }
+
+	def calc_point(weights)
+  	# "weights"=>{"1"=>{"2"=>"true"}, "2"=>{"3"=>"true"}, "3"=>{"3"=>"true"}, "4"=>{"2"=>"true"}, "5"=>{"2"=>"true"}, "6"=>{"3"=>"true"}}
+		self.open_data.inject(0) do |sum, i|
+			category_id = i.indicator_source.indicator.category.id.to_s
+			if weights[category_id].present?
+				sum + i.deviation_value * weights[category_id].keys.first.to_i
+			else
+				sum + 0
+			end
+		end
+	end
+
 end
